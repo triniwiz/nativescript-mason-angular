c9485263927ca14e2ac474e1d9b259dabddbf72f
Osei Fortune <fortune.osei@yahoo.com>
2025-12-18 00:37:01 -0400

    chore: add mason

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,39 @@ import {
   bootstrapApplication,
   provideNativeScriptHttpClient,
   provideNativeScriptRouter,
+  registerElement,
   runNativeScriptAngularApp,
-} from '@nativescript/angular';
-import { provideZonelessChangeDetection } from '@angular/core';
-import { withInterceptorsFromDi } from '@angular/common/http';
-import { routes } from './app/app.routes';
-import { AppComponent } from './app/app.component';
+} from "@nativescript/angular";
+import { provideZonelessChangeDetection } from "@angular/core";
+import { withInterceptorsFromDi } from "@angular/common/http";
+import { routes } from "./app/app.routes";
+import { AppComponent } from "./app/app.component";
+import {
+  B,
+  Code,
+  Div,
+  H1,
+  H2,
+  H3,
+  H4,
+  P,
+  Span,
+} from "@triniwiz/nativescript-masonkit/web";
+import { Button, Img, View } from "@triniwiz/nativescript-masonkit";
+
+registerElement("view", () => View);
+registerElement("div", () => Div);
+registerElement("img", () => Img);
+registerElement("text", () => Text);
+registerElement("p", () => P);
+registerElement("sspan", () => Span);
+registerElement("b", () => B);
+registerElement("h1", () => H1);
+registerElement("h2", () => H2);
+registerElement("h3", () => H3);
+registerElement("h4", () => H4);
+registerElement("code", () => Code);
+registerElement("sbutton", () => Button);
 
 runNativeScriptAngularApp({
   appModuleBootstrap: () => {
